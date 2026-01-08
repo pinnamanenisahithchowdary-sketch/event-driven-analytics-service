a1aa78c5e0c177904b9581c18fa03fbd5f9b1178
pinnamanenisahithchowdary-sketch <pinnamanenisahithchowdary@gmail.com>
2026-01-08 01:08:24 -0800

    Add Spring Boot main application

---
+++ src/main/java/com/analytics/EventDrivenAnalyticsServiceApplication.java
@@ -0,0 +1,11 @@
+package com.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenAnalyticsServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventDrivenAnalyticsServiceApplication.class, args);
+    }
+}
